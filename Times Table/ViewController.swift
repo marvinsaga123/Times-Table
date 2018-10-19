612c6547b603bfd6ee8271a40e2e9137257648b2
Marvin Sagastume <msagastume123@gmail.com>
2018-10-19 01:15:42 -0400

    Developed app

--- Times Table/ViewController.swift
+++ Times Table/ViewController.swift
@@ -8,7 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var timesTable: UITableView!
+    @IBOutlet weak var slider: UISlider!
+    @IBAction func sliderChanged(_ sender: Any) {
+        timesTable.reloadData()
+    }
+    
+    @available(iOS 2.0, *)
+    internal func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
+    }
+
+    @available(iOS 2.0, *)
+    internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = "[ \(String(indexPath.row + 1)) * \(String(Int(slider.value))) ] \(String((indexPath.row + 1) * Int(slider.value)))"
+        return cell
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +37,4 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
